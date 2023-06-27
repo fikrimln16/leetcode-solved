a37cabe849dad8fd55df40aa2b8709ea9641d54f
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-06-27 18:25:36 +0700

    Time: 78 ms (23.83%), Space: 43.4 MB (91.12%) - LeetHub

---
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isFascinating = function(n) {
+
+   //gabungkan jadi satu
+   let res = `${n}${n*2}${n*3}`.split('')
+
+   if(res.includes('0')){
+      return false
+   }
+   if(res.length !== 9){
+      return false
+   }
+   if ((new Set(res)).size !== 9) {
+      return false
+   }
+
+   return true
+};
