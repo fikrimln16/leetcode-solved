a24690fc311270672b0807a30b8deb5389e5e278
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-06-27 10:09:13 +0700

    Time: 72 ms (30.45%), Space: 43.2 MB (99.82%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 1) continue;
+
+    if (
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1;
+      n--;
+    }
+  }
+
+  return n <= 0;
+};
