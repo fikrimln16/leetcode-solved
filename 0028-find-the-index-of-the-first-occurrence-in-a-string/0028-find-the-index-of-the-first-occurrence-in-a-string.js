01dcf9adea224813d23410fa18a57bb2673ece36
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-06-28 13:26:45 +0700

    Time: 61 ms (44.73%), Space: 41.3 MB (95.71%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle === "") {
+    return 0;
+  }
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    let found = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        found = false;
+        break;
+      }
+    }
+    if (found) {
+      return i;
+    }
+  }
+
+  return -1;
+};
