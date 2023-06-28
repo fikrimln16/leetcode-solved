ce49ca30fedb54ccb1f77499c321178e9b6a9573
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-06-28 12:51:35 +0700

    Time: 68 ms (74.55%), Space: 45.8 MB (20.75%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  //jika jumlah tidak sama maka false
+  if (s.length != t.length) return false;
+
+  let temp_1 = [];
+  let temp_2 = [];
+
+  for (let i = 0; i < s.length; i++) {
+    temp_1.push(s.indexOf(s[i]));
+    temp_2.push(t.indexOf(t[i]));
+
+    //ketika tidak sama, berarti terdapat item yang sama, karena akan mengeluarkan index yang sudah ada
+    if (temp_1[i] != temp_2[i]) return false;
+  }
+  return true;  
+};
