6602dc4e5bc8d9c1743d5e520c64c7f06482c53b
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-02 11:15:51 +0700

    Time: 101 ms (50.61%), Space: 46.7 MB (34.29%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var findMatrix = function(nums) {
+  let map = new Map();
+  let answer = [];
+  
+  //counter
+  for (num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  map.forEach((value, key) => {
+    for (let i = 0; i < value; i++) {
+      let temp = answer[i]
+      temp
+      if (!temp) {
+        answer.push([]);
+      }
+      answer[i].push(key); 
+    }
+  });
+  return answer;
+};
