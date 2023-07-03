6b9b15debc8493020a375c2542c42c5a228865e0
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-03 19:13:33 +0700

    Time: 53 ms (81.63%), Space: 41.9 MB (61.76%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+  const res = [];
+  
+  let i = 0;
+  while (i < nums.length) {
+    let start, last;
+    start = nums[i];
+  
+    //melakukan perulangan dari index awal, jika index selanjutnya lebih dari i+1, maka berhenti
+    while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
+      i++;
+    }
+
+    last = nums[i];
+    //jika awal dan akhirnya sama, berarti hanya satu index
+    if (start == last) {
+      res.push(start + "");
+    } else {
+      res.push(start + "->" + last);
+    }
+
+    i++;
+  }
+
+  return res;
+};
