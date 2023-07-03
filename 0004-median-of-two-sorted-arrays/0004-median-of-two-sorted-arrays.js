875556cc13de6010c3c19962c9bbb9668c544be4
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-03 19:19:57 +0700

    Time: 112 ms (42.80%), Space: 49.4 MB (16.80%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+   let merged = [...nums1, ...nums2].sort((a, b) => a - b);
+   merged.sort((a, b) => a-b)
+   let total = merged.length
+
+   if(total % 2 === 0){
+      let mid = total / 2;
+      return med = (merged[mid] + merged[mid-1]) / 2
+   } else {
+      return merged[Math.floor(total/2)]
+   }
+};
