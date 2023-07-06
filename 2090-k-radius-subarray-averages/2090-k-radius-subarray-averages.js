c54469172197d5e7cee3cbc1a7ac6068be56ddec
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-06 21:35:17 +0700

    Time: 414 ms (26.07%), Space: 92.6 MB (26.33%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
@@ -4,18 +4,24 @@
  * @return {number[]}
  */
 var getAverages = function(nums, k) {
-  if (k === 0) return nums;
-
-  let arr = new Array(nums.length).fill(-1);
-
-  for (let i = k; i < nums.length - k; i++) {
-    let sum = 0;
-    for (let j = i - k; j <= i + k; j++) {
-      sum += nums[j];
+    
+  const prefixSum = new Array(nums.length).fill(-1);
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0) {
+      prefixSum[i] = nums[i];
+    } else {
+      prefixSum[i] = nums[i] + prefixSum[i - 1];
     }
-    let avg = Math.floor(sum / (2 * k + 1));
-    arr[i] = avg;
   }
-
-  return arr;
+  console.log(prefixSum) // [7,11,14,23,24,32,37,39,45]
+  
+  const res = new Array(nums.length).fill(-1);
+  for (let i = k ; i <= (nums.length - 1) - k; i++) {
+      if (i === k) {
+          res[i] = Math.floor(prefixSum[i + k] / (k*2+1));
+      } else {
+          res[i] = Math.floor((prefixSum[i + k] - prefixSum[i - k - 1]) / (k*2+1));
+      }
+  }
+  return res;
 };
