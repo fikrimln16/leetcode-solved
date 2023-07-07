56c71826ebd03788c9f702cbf50573c9d37e8de6
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-07 19:11:34 +0700

    Time: 59 ms (83.36%), Space: 42.4 MB (48.61%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -3,27 +3,25 @@
  * @return {number}
  */
 
-function findSubarrays(arr) {
-  const subarrays = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      const subarray = arr.slice(i, j + 1);
-      subarrays.push(subarray);
-    }
-  }
-
-  return subarrays;
-}
-
 var sumOddLengthSubarrays = function(arr) {
-    return findSubarrays(arr).map((element) => {
-    if (element.length % 2 !== 0) {
-      return element
-    }
-  }).filter((element) => element!=undefined).reduce((accumulator, currentArray) => {
-    return accumulator + currentArray.reduce((subAccumulator, currentValue) => {
-      return subAccumulator + currentValue;
-    }, 0);
-  }, 0);
-};
+  const n = arr.length;
+  let sum = 0;
+  
+  if (n === 1) return arr[0];
+  
+  // Turn arr into a prefix sum array
+  for (let i = 1; i < n; i++)
+      arr[i] += arr[i-1];
+  
+  let numOfSlidingWindows = Math.ceil(n/2);
+  
+  for (let i = 0; i < numOfSlidingWindows; i++) {
+      for (let j = i*2; j < n; j++) {
+          if (j-(i*2)-1 < 0) {
+              sum += arr[j];
+          }
+          else sum += arr[j] - arr[j-(i*2)-1]
+      }
+  }
+  return sum;
+};  
