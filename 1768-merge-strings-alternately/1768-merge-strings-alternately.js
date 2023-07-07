6b919781ed48d4b8d2181313c36e30aab92d3c59
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-07 10:26:54 +0700

    Time: 69 ms (12.99%), Space: 44.1 MB (5.60%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+   let res = ''
+   for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+      if(word1[i]) res+= word1[i]
+      if(word2[i]) res+= word2[i]
+   }
+   return res
+};
