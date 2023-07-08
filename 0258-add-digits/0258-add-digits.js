bd3713146969d6cd2c1b911d92198f5590dd100b
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-08 20:22:35 +0700

    Time: 64 ms (86.26%), Space: 43.8 MB (68.03%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,21 +3,15 @@
  * @return {number}
  */
 const addDigits = (num) => {
-  if (num === 0) {
-    return 0;
-  }
-  
-  while (num >= 10) {
-    let digitSum = 0;
-    
-    while (num > 0) {
-      let temp = num % 10
-      digitSum += temp;
-      num = Math.floor(num / 10);
-    }
-    
-    num = digitSum;
-  }
-  
-  return num;
-};
+   let result = Infinity;
+
+   if (num === 0) return 0;
+
+   while (true) {
+     if (result < 10) return result;
+     digVal = (num % 10) + Math.floor(num / 10); 
+     result = digVal;
+     result
+     num = digVal;
+   }
+ };
