0d6090b1d6296ef80710852ec0e84a69e4f9453e
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-08 13:33:47 +0700

    Time: 76 ms (51.16%), Space: 45.5 MB (62.79%) - LeetHub

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxSum = function(grid) {
+   let max = 0
+
+   for (let row = 0; row < grid.length-2; row++) {
+      for (let col = 0; col < grid[0].length-2; col++) {
+         max = Math.max(max, grid[row][col] + grid[row][col+1] + grid[row][col+2] + grid[row+1][col+1] + grid[row+2][col] + grid[row+2][col+1] + grid[row+2][col+2])
+      }      
+   }
+
+   return max
+};
