0b97c3b2b76b0c09b9941e829a77c7fc7f029aac
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-16 11:18:20 +0700

    Time: 79 ms (45.51%), Space: 45.8 MB (22.17%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  //melalukan rumus prefix sum
+  let prefixSum = [];
+  prefixSum[0] = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    prefixSum[i] = prefixSum[i-1] + nums[i]
+  }
+
+  //melakukan perulangan sampai mendapatkan value yang sama
+  for (let i = 0; i < nums.length; i++) {
+    let a = prefixSum[i-1];
+    if(!a) a=0
+    const b = prefixSum[nums.length - 1] - prefixSum[i+1-1]
+    if(a==b) return i
+  }
+
+  //kembalikan -1 jika tidak ada yang sama di perulangan sblmnya
+  return -1;
+};
