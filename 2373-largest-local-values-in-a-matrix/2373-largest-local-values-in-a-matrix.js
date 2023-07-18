97fdfaec19b7cb35c9cda4cefacff78f05523d60
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-18 20:49:34 +0700

    Time: 82 ms (68.38%), Space: 45.9 MB (66.67%) - LeetHub

---
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var largestLocal = function(grid) {
+   
+   let lokal = []
+   for (let row = 0; row < grid.length - 2; row++) {
+      let temp = []
+      for (let col = 0; col < grid.length - 2; col++) {
+         temp.push(0)
+      }
+      lokal.push([...temp])
+   }
+
+   for (let row = 0; row < grid.length - 2; row++) {
+      for (let col = 0; col < grid.length - 2; col++) {
+         lokal[row][col] = Math.max(
+            grid[row][col], grid[row][col+1], grid[row][col+2],
+            grid[row+1][col], grid[row+1][col+1], grid[row+1][col+2],
+            grid[row+2][col], grid[row+2][col+1], grid[row+2][col+2]
+         )
+      }
+   }
+   
+   return lokal
+
+};
