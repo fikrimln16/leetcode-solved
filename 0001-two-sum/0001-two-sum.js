01305d21aa3c276251fdbd4a16d0e92f1458c543
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-21 18:38:33 +0700

    Time: 58 ms (92.13%), Space: 43.2 MB (29.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let res = []
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i+1; j < nums.length; j++) {
-      let sum = nums[i] + nums[j]
-      if(sum === target){
-        res.push(i, j)
-      }
-    }
-    
-  }  
+   let map = new Map()
 
-  return res
+   for (let i = 0; i < nums.length; i++) {
+      let cur = target-nums[i]
+      if(map.has(cur)){
+         return [map.get(cur), i]
+      } 
+         
+      map.set(nums[i], i)
+      
+   }
 };
