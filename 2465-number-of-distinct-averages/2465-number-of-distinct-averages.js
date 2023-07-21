fcb7ad611c6249842909305a3820f59060023242
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-21 19:04:46 +0700

    Time: 54 ms (78.07%), Space: 41.8 MB (76.47%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.js
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.js
@@ -7,10 +7,8 @@ var distinctAverages = function(nums) {
    let sorted = nums.sort((a, b) => a-b)
    let set = new Set()
    while(sorted.length !== 0){
-      let avg = (sorted[0] + sorted[sorted.length-1]) / 2
+      let avg = (sorted.shift() + sorted.pop()) / 2
       set.add(avg)
-      sorted.shift()
-      sorted.pop()
    }
 
    return set.size
