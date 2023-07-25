b86ca2646c9cb9b69cf0eb0da1246d10eae36cdf
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-07-25 16:50:30 +0700

    Time: 85 ms (54.22%), Space: 45.3 MB (32.53%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
@@ -3,6 +3,21 @@
  * @param {number[]} queries
  * @return {number[]}
  */
+
+function binarySearch(prefixSum, target){
+  let left = 0
+  let right = prefixSum.length - 1
+  while(left<=right){
+    let mid = Math.floor((left+right) / 2)
+    if(prefixSum[mid] <= target){
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+  return left
+}
+
 var answerQueries = function (nums, queries) {
   let sorted = nums.sort((a, b) => a - b);
 
@@ -12,39 +27,10 @@ var answerQueries = function (nums, queries) {
     prefixSum[i] = sorted[i] + prefixSum[i - 1];
   }
 
-
-  // function binarySearch(prefixSum, target){
-  //   let left = 0
-  //   let right = prefixSum.length - 1
-
-  //   while(left<=right){
-  //     let mid = Math.floor((left+right) / 2)
-
-  //     if(prefixSum[mid] <= target){
-  //       left = mid + 1
-  //     } else {
-  //       right = mid - 1
-  //     }
-  //   }
-
-  //   return left
-  // }
-
-  // let res = []
-  // queries.forEach((queri) => {
-  //   res.push(binarySearch(prefixSum, queri))
-  // })
-
   let res = []
   queries.forEach((queri) => {
-    let count = 0;
-    let i = 0;
-    while (prefixSum[i] <= queri) {
-      count++;
-      i++;
-    }
-    res.push(count);
-  });
+    res.push(binarySearch(prefixSum, queri))
+  })
 
   return res;
 };
