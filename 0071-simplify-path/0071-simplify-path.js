0d92a068b9b642df99cad88b4a73a38b834fa684
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-08-01 10:44:12 +0700

    Time: 53 ms (95.86%), Space: 43.9 MB (81.86%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,46 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    this.stack.push(item);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack kosong";
+    }
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  join(){
+    return this.stack.join("/")
+  }
+}
+
+const simplifyPath = function (path) {
+  const stack = new Stack();
+
+  let split = path.split('/')
+  split
+
+  for(const arr of split){
+    if(arr === "" || arr === ".") continue
+
+    if(arr === ".."){
+      stack.pop()
+    } else {
+      stack.push(arr)
+    }
+  }
+  return "/" + stack.join()
+};
