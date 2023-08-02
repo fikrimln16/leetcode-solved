ec09ca3af9e2ff092760079f49105707de693a4a
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-08-02 20:51:59 +0700

    Time: 88 ms (92.76%), Space: 61.1 MB (15.97%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -1,17 +1,48 @@
-/**
- * @param {string} s
- * @return {string}
- */
-var removeStars = function(s) {
-    let stack = []
+class Stack{
+    constructor(){
+        this.stack = []
+    }
 
-    s.split("").forEach((element) => {
-        if(element != "*"){
-            stack.push(element)
-        } else {
-            stack.pop()
+    push(element){
+        this.stack.push(element)
+    }
+
+    pop(){
+        if(this.isEmpty()) return "Stack Kosong"
+        return this.stack.pop()
+    }
+
+    isEmpty(){
+        return this.stack.length === 0
+    }
+
+    peek(){
+        return this.stack[this.stack.length - 1]
+    }
+
+    print(){
+        let string = ""
+        for(let i = 0; i<this.stack.length;i++){
+            string += this.stack[i]
         }
-    })
+        return string
+    }
 
-    return stack.join("")
+}
+
+
+var removeStars = function(s) {
+
+    let split = s.split("")
+
+    let stack = new Stack()
+    for(let char of split){
+        if(char === "*"){
+            stack.pop()
+        } else {
+            stack.push(char)
+        }
+    }
+    return stack.print()
+
 };
