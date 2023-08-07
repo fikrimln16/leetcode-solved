f71d3b0e2c639281e5217ef7b56b81f3c1e93814
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-08-07 10:38:26 +0700

    Time: 49 ms (90.28%), Space: 41.5 MB (88.80%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -7,7 +7,8 @@ var removeElement = function(nums, val) {
    let res = 0;
    for(let i = 0; i < nums.length; i++){
       if(nums[i] != val){
-         nums[res++] = nums[i]
+         nums[res] = nums[i];
+         res++
       }
    }
 
